824a4ea6e246493feca6ad6ac4a0069fd21d79f4
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-06-05 19:50:32 -0300

    Add authentication service
    The authentication service provides sign in/sign out using Firebase
    as the backend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { App } from './app.component';
 import { SignInPage } from '../pages/signin/signin';
+import { AuthService } from '../providers/auth-service/auth-service';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC7y1GwD18kgl6KSNWwTlNnM2sMnVLPqbg",
+  authDomain: "knot-258c9.firebaseapp.com",
+  databaseURL: "https://knot-258c9.firebaseio.com",
+  projectId: "knot-258c9",
+  storageBucket: "knot-258c9.appspot.com",
+  messagingSenderId: "373290372028"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +30,8 @@ import { SignInPage } from '../pages/signin/signin';
     IonicModule.forRoot(App, {
       scrollAssist: false,
       autoFocusAssist: false
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,7 +41,9 @@ import { SignInPage } from '../pages/signin/signin';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth,
+    AuthService
   ]
 })
 export class AppModule {}
