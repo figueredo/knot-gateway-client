a63067cceffbac9137d6b256f1a755e285976284
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-06-06 17:24:11 -0300

    Show allowed/broadcasting devices in devices page
    The devices page show the allowed and broadcasting devices in two
    separate lists. In the allowed list, it will also show whether the
    device is online or offline.

--- src/pages/devices/devices.ts
+++ src/pages/devices/devices.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { DevicesService } from '../../providers/devices-service/devices-service';
 
 @IonicPage()
 @Component({
   selector: 'page-devices',
   templateUrl: 'devices.html',
 })
-export class DevicesPage {
+export class DevicesPage implements OnInit {
 
-  constructor() {
+  allowedDevices: Observable<any[]>;
+  notAllowedDevices: Observable<any[]>;
+  hasAllowedDevices: Observable<boolean>;
+
+  constructor(private devicesService: DevicesService) {
+  }
+
+  ngOnInit() {
+    this.allowedDevices = this.devicesService.allowedDevices;
+    this.notAllowedDevices = this.devicesService.notAllowedDevices;
+    this.hasAllowedDevices = this.allowedDevices
+      .map(devices => devices.length > 0);
   }
 
 }
