de1a30d337d52e69dda0bcba9572a3cb26a0f469
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-06-06 14:05:55 -0300

    Authenticate the user with the back-end
    Connects the sign in page with the authentication service.

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
+
+import { AuthService } from '../../providers/auth-service/auth-service';
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html'
 })
 export class SignInPage {
+  private loading: Loading;
+  private signInForm: FormGroup;
 
-  constructor() {
-
+  constructor(private navCtrl: NavController, private loadingCtrl: LoadingController, private alertCtrl: AlertController, private formBuilder: FormBuilder, private auth: AuthService) {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
+  signIn(credentials) {
+    this.showLoading();
+    this.auth.signIn(credentials)
+      .then(() => this.onSignInSuccess(),
+            () => this.onSignInFailure());
+  }
+
+  private onSignInSuccess(): void {
+    this.loading.dismiss();
+  }
+
+  private onSignInFailure(): void {
+    this.loading.dismiss();
+    this.showError();
+  }
+
+  private showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Signing in...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  private showError(): void {
+    const alert = this.alertCtrl.create({
+      message: 'Invalid credentials. Try again.',
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
 }
